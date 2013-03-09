ffe00e881529d588d5a58e796a18e588886f2ec4
Eric Menendez <ericmenendez@gmail.com>
2013-03-08 22:13:45 -0500

    Added code & file wireframes

--- index.php
+++ index.php
@@ -6,9 +6,33 @@ noCache();
 
 $db = dbConnect();
 
-if(isset($_GET['id']))
+if(isset($_GET['update']))
 {
+	// Page being called from XMLHttpRequest()
+	// Update database with location using ID
+	// Return a confirmation to user
+	$db->query("update gps set loc='" . $_GET['loc'] ."' where id=" . $_GET['update']);
+	echo('Position received.');
+}
+else
+{
+	// Page being visited in browser
+
+	// Use mod_rewrite to rewrite URL as ID
+
+	if(isset($_GET['id']))
+	{
+		// Base64-decode ID
+	}
+	else
+	{
+		// Use IP address to look up or generate ID
+	}
+
 	// Get location
+	// Use position tracking
+	// Graceful error handling
+	// Minimize initial download size
 	?><!DOCTYPE html>
 	<htmk><head><script type="text/javascript">
 	function getPos(position) {
@@ -36,12 +60,7 @@ if(isset($_GET['id']))
 		}
 	};
 	</script></head><body><span id="startLat">?</span></body></html><?php
-}
-else if(isset($_GET['update']))
-{
-	// update database with loc
-	$db->query("update gps set loc='" . $_GET['loc'] ."' where id=" . $_GET['update']);
-	echo('Position received.');
+
 }
 
 ?>
--- ~admin/index.php
+++ ~admin/index.php
@@ -16,6 +16,8 @@ $db = dbConnect();
 <body>
 	<div id="header"><div id="logo"><img src="asrclogo.gif" alt="ASRC" /></div>
 		<div id="sms">
+			<!-- Prompt for SMS text and phone number here. Use drop-down list pf providers. Use a message preview to display character limit -->
+			<!-- Enter SMS email address (e.g. ##########@vtext.com or ##########@txt.att.net): <input type="text" name="email" /> -->
 		</div>
 	</div>
 	<div id="table">
@@ -33,42 +35,21 @@ if(isset($_POST['submit']))
 {
 	// Add email to database and get ID
 	// Send email
-	// Redirect to check page
 	$result = $db->query("insert into gps (email) values ('" . $_POST['email'] . "')");
 	$result = $db->query("select id from gps where email='" . $_POST['email'] . "' order by id desc");
 	$array = $result->fetch_assoc();
 	mail($_POST['email'], 'Tap link to send location to SAR', "http://myasrc.dreamhosters.com/gps/index.php?id=" . $array['id']);
-	header("Location: index.php?check=" . $array['id']);
 }
-else if(isset($_GET['check']))
+
+// Get list of phones and last location for each
+// Display in table with link to Google maps
+$result = $db->query("select * from gps where id=" . $_GET['check']);
+$array = $result->fetch_assoc();
+if($array['loc'] != '')
 {
-	// Check to see if loc is in database
-	// display with link to google maps
-	$result = $db->query("select * from gps where id=" . $_GET['check']);
-	$array = $result->fetch_assoc();
-	if($array['loc'] != '')
-	{
-		// Location found
-		?><!DOCTYPE html>
-		<html><body>Location: <a href="http://maps.google.com/maps?q=<?php echo(urlencode($array['loc'])); ?>"><?php echo($array['loc']); ?></a></body></html><?php
-	}
-	else
-	{
-		// Wait 5 sec
-		?><!DOCTYPE html>
-		<html><head><meta http-equiv="refresh" content="5" /></head><body>Location not found. Waiting...</body></html><?php
-	}
-}
-else
-{
-	// Ask for SMS email
-	// POST to submit
+	// Location found
 	?><!DOCTYPE html>
-	<html><body><form method="POST" action="index.php">
-	Enter SMS email address (e.g. ##########@vtext.com or ##########@txt.att.net): <input type="text" name="email" />
-	<input type="submit" name="submit" /></form>
-	</body></html>
-	<?php
+	<html><body>Location: <a href="http://maps.google.com/maps?q=<?php echo(urlencode($array['loc'])); ?>"><?php echo($array['loc']); ?></a></body></html><?php
 }
 
 ?>
