b20056f477807738b12bb9480283008cb2a23fae
Eric Menendez <ericmenendez@gmail.com>
2013-03-09 02:21:56 -0500

    Added SQL datatabase statements to repo

--- ~admin/inc.php
+++ ~admin/inc.php
@@ -49,4 +49,31 @@ function base64url_decode($data)
 	return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
 }
 
+/*
+ * Detailed Database Structure
+ *
+
+CREATE TABLE IF NOT EXISTS `gps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone_id` int(11) NOT NULL,
+  `loc` varchar(64) DEFAULT NULL,
+  `altitude` double NOT NULL,
+  `accuracy` double NOT NULL,
+  `altitudeAccuracy` double NOT NULL,
+  `heading` double NOT NULL,
+  `speed` double NOT NULL,
+  `location_time` timestamp NULL DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
+
+CREATE TABLE IF NOT EXISTS `phones` (
+  `phone_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `email_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`phone_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+
+ */
+
 ?>
