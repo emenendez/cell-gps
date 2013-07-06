44f2843afd2f2022755a7414996f514475c23bee
Eric Menendez <ericmenendez@gmail.com>
2013-07-06 19:30:06 -0400

    Update jQuery to 1.10.2

--- ~admin/index.php
+++ ~admin/index.php
@@ -18,7 +18,7 @@ $db->query('delete from phones where email_time<(now() - interval 1 week)');
 	<!-- <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
 	<!-- <link rel="stylesheet" href="css/bootstrap-responsive.min.css" /> -->
 	<link rel="stylesheet" href="css/style.css" />
-	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery-1.10.2.min.js"></script>
 	<!-- <script src="js/bootstrap.min.js"></script> -->
 	<script src="js/script.js"></script>
 </head>
