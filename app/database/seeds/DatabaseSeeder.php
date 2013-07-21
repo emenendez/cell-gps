9fd46ec6dc5ea8ac0217fec718c15bfca26359f9
Eric Menendez <ericmenendez@gmail.com>
2013-07-21 18:46:49 -0400

    Add seed data

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,36 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('AdminPageSeeder');
 	}
 
+}
+
+class AdminPageSeeder extends Seeder {
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		$user = User::create(array(
+			'username' => 'tester',
+			'email' => 'ericmenendez@gmail.com',
+			'organization' => 'Test Organization',
+			'password' => Hash::make('password')
+			));
+
+		$phone = Phone::create(array(
+			'user_id' => $user->id,
+			'email' => '2027310827@vtext.com'
+			));
+
+		$location = Location::create(array(
+			'phone_id' => $phone->id,
+			'location' => 'test location',
+			'altitude' => 0,
+			'accuracy' => 0,
+			'altitudeAccuracy' => 0,
+			'heading' => 0,
+			'speed' => 0
+			));
+	}
 }
