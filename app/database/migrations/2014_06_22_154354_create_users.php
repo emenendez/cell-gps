dbbc4ace3757d2f910ffe711d633a97b4dec7545
Eric Menendez <ericmenendez@gmail.com>
2014-06-22 16:07:48 -0400

    migrations

---
+++ app/database/migrations/2014_06_22_154354_create_users.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create users table
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('email', 320);
+			$table->string('organization', 320);
+			$table->string('password', 64);
+			$table->timestamps();
+			$table->timestamp('last_login');
+		});
+
+		// Add Guest user
+		DB::insert('insert into users (id, email, organization, password) values (?, ?, ?, ?)', array(1, 'Guest', '', Hash::make('')));
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop users table
+		Schema::drop('users');
+	}
+
+}
---
+++ app/database/migrations/2014_06_22_160614_create_phones.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhones extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create phones table
+		Schema::create('phones', function($table){
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->string('email', 320);
+			$table->timestamps();
+
+			$table->foreign('user_id')
+				  ->references('id')->on('users')
+				  ->onUpdate('cascade')
+				  ->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop phones table
+		Schema::drop('phones');
+	}
+
+}
