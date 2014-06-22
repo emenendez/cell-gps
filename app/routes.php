ee8c04ec08878e0db4dc349a9ae512d7537c22b1
Eric Menendez <ericmenendez@gmail.com>
2014-06-22 16:18:41 -0400

    routes

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,78 @@
 |
 */
 
-Route::get('/', function()
+// List of providers
+View::composer('master', function($view)
 {
-	return View::make('hello');
+	$providers = array(
+		'' => 'Other',
+		'message.alltel.com' => 'Alltel',
+		'txt.att.net' => 'AT&T',
+		'txt.bellmobility.ca' => 'Bell Canada',
+		'myboostmobile.com' => 'Boost Mobile',
+		'sms.mycricket.com' => 'Cricket',
+		'messaging.nextel.com' => 'Nextel (now Sprint Nextel)',
+		'pcs.ntelos.com' => 'nTelos',
+		'messaging.sprintpcs.com' => 'Sprint (now Sprint Nextel)',
+		'tmomail.net' => 'T-Mobile',
+		'email.uscc.net' => 'US Cellular',
+		'vtext.com' => 'Verizon',
+		'vmobl.com' => 'Virgin Mobile'
+		);
+    $view->with('providers', $providers);
+	$view->with('provider', Input::old('provider', ''));
+	$view->with('phone', Input::old('phone', ''));
+	$view->with('gateway', Input::old('gateway', ''));
+	$view->with('subject', Input::old('subject', ''));
+	$view->with('message', Input::old('message', ''));
+});
+
+// Route models
+Route::model('phone', 'Phone');
+
+// Routes that require authentication
+Route::group(array('before' => 'auth'), function()
+{
+
+	Route::get('/', array('as' => 'index', 'uses' => 'PhoneController@showPhones'));
+
+	// Display all locations for a phone
+	Route::get('phone/{phone}', array('as' => 'phone', 'uses' => 'PhoneController@showPhone'))->where('phone', '[0-9]+');
+	
+	// Routes that also require csrf
+	Route::group(array('before' => 'csrf'), function()
+	{
+		// Send SMS
+		Route::post('/', array('as' => 'send-sms', 'uses' => 'PhoneController@sendSMS'));
+	});
+
+});
+
+// Show login form
+Route::get('login', array('as' => 'login', 'uses' => 'UserController@showLogin'));
+
+Route::get('login/guest', array('as' => 'login-guest', function() {
+	// Log in as guest
+	Auth::loginUsingId(1);
+	return Redirect::intended('/');
+}));
+
+// Show registration form
+Route::get('signup', array('as' => 'register', 'uses' => 'UserController@showRegistration'));
+
+Route::get('logout', array('as' => 'logout', function() {
+	// Log out
+	Auth::logout();
+	return Redirect::route('index');
+}));
+
+// Routes that require csrf
+Route::group(array('before' => 'csrf'), function()
+{
+	// Process login
+	Route::post('login', array('as' => 'login-submit', 'uses' => 'UserController@processLogin')); 
+
+	// Process registration form
+	Route::post('signup', array('as' => 'register-submit', 'uses' => 'UserController@processRegistration'));
+
 });
