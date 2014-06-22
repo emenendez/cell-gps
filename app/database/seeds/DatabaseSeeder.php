ba7c136e1f3b11105e315eabdcf429f8eef5cbc2
Eric Menendez <ericmenendez@gmail.com>
2014-06-22 16:20:25 -0400

    seeds

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,27 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('AdminPageSeeder');
 	}
 
+}
+
+class AdminPageSeeder extends Seeder {
+	public function run()
+	{
+		$phone = Phone::create(array(
+			'user_id' => 1,
+			'email' => '2027310827@vtext.com'
+			));
+
+		$location = Location::create(array(
+			'phone_id' => $phone->id,
+			'location' => 'test location',
+			'altitude' => 0,
+			'accuracy' => 0,
+			'altitudeAccuracy' => 0,
+			'heading' => 0,
+			'speed' => 0
+			));
+	}
 }
