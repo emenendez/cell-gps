0570072d2aebde00d72d4b0da0e6978495e6e981
Eric Menendez <ericmenendez@gmail.com>
2014-06-26 18:20:39 -0400

    Remove non-framework files

--- app/start/global.php
+++ app/start/global.php
@@ -80,6 +80,15 @@ App::down(function()
 
 require app_path().'/filters.php';
 
-// Add global includes
-require app_path().'/../inc.php';
+/**
+ * Global utility functions
+ */
+function base64url_encode($data)
+{
+	return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+}
 
+function base64url_decode($data)
+{
+	return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
+}
