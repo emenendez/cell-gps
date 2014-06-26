21321659cdd821e9c8487f9a88fa2bfe422184e6
Eric Menendez <ericmenendez@gmail.com>
2014-06-26 19:35:01 -0400

    Set date fields on models

--- app/models/Location.php
+++ app/models/Location.php
@@ -4,6 +4,18 @@ class Location extends Eloquent {
 
 	protected $touches = array('phone');
 
+	/**
+	 * Date fields
+	 */
+	public function getDates()
+	{
+		return array(
+			'created_at',
+			'updated_at',
+			'location_time',
+			);
+	}
+
 	public function phone() {
 		return $this->belongsTo('Phone');
 	}
