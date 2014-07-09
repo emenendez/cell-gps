d87417fbf8dd66e551e0729209ba3d3bf4ced42d
Eric Menendez <ericmenendez@gmail.com>
2014-07-09 19:20:29 -0400

    Add placeholder plugin to templates

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -106,6 +106,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="{{ asset('js/jquery-1.10.2.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.placeholder.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/libphonenumber.js') }}"></script> {{-- from https://github.com/nathanhammond/libphonenumber/ --}}
     <script src="{{ asset('js/script.js') }}"></script>
