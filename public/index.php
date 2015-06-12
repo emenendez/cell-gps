e38e8f87d2c547cc5b739a0a4bcdcc5d2462ed58
Eric Menendez <ericmenendez@gmail.com>
2015-06-12 13:54:24 -0400

    globals

--- public/index.php
+++ public/index.php
@@ -34,6 +34,19 @@ require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+/**
+ * Global utility functions
+ */
+function base64url_encode($data)
+{
+	return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+}
+
+function base64url_decode($data)
+{
+	return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
+}
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
