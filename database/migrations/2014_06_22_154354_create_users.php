2b46b32c3e8f9a55cbcf0d7e8be2162616b65385
Eric Menendez <ericmenendez@gmail.com>
2015-06-12 13:49:50 -0400

    database

---
+++ database/migrations/2014_06_22_154354_create_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create users table
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('email', 320);
+			$table->string('organization', 320);
+			$table->string('password', 64);
+			$table->timestamps();
+			$table->timestamp('last_login');
+			$table->string('remember_token', 100)->nullable();
+		});
+
+		// Add Guest user
+		DB::insert('insert into users (id, email, organization, password) values (?, ?, ?, ?)', array(User::guestId(), 'Guest', '', Hash::make('')));
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop users table
+		Schema::drop('users');
+	}
+
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,29 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('AdminPageSeeder');
 	}
 
+}
+
+class AdminPageSeeder extends Seeder {
+	public function run()
+	{
+		$phone = Phone::create(array(
+			'user_id' => User::guestId(),
+			'email' => '1234567890@vtext.com',
+			'created_at' => '2014-06-01 00:00:00',
+			));
+
+		$location = Location::create(array(
+			'phone_id' => $phone->id,
+			'location' => 'test location',
+			'altitude' => 0,
+			'accuracy' => 0,
+			'altitudeAccuracy' => 0,
+			'heading' => 0,
+			'speed' => 0,
+			'created_at' => '2014-07-01 00:00:00',
+			));
+	}
 }
