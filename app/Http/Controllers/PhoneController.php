33f565641babe185e7c7c0a8f74251987b886104
Eric Menendez <ericmenendez@gmail.com>
2015-06-16 16:04:09 -0400

    unneeded phone methods

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -135,27 +135,4 @@ class PhoneController extends Controller {
     }
   }
 
-
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store()
-  {
-    //
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-    //
-  }
-
 }
