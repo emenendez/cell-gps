115ffbdd6cac4230c9a6bae5dc96b3963dd4392d
Chris Thompson <christhompsontldr@gmail.com>
2020-06-11 23:25:47 -0400

    Updating packages for L7

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -94,6 +94,7 @@ return [
     'not_regex' => 'The :attribute format is invalid.',
     'numeric' => 'The :attribute must be a number.',
     'password' => 'The password is incorrect.',
+    'phone' => 'The :attribute field contains an invalid number.',
     'present' => 'The :attribute field must be present.',
     'regex' => 'The :attribute format is invalid.',
     'required' => 'The :attribute field is required.',
