af59a4668be979cf2bdc72f650cb00eeaedb3715
Chris Thompson <christhompsontldr@gmail.com>
2020-06-15 14:01:01 -0400

    Allow sessions to drop off

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -26,8 +26,6 @@ class PhoneController extends Controller
             }
 
             session(['phone' => $phone->token]);
-
-            return redirect()->route('phones.show', $phone);
         }
 
         return redirect()->route('phones.show', session('phone'));
@@ -46,6 +44,8 @@ class PhoneController extends Controller
 
         // session / url mismatch
         if (session('phone') !== $phone->token) {
+            session()->forget('phone');
+
             return redirect()->route('home');
         }
 
@@ -73,53 +73,4 @@ class PhoneController extends Controller
 
         return 'Your location has been received by Search &amp; Rescue.';
     }
-
-
-
-
-
-
-    /**
-     * Purge old phones from database
-     *
-     * @todo: cthompson move to Artisan command/schedule
-     */
-    private function purgePhones()
-    {
-        // Delete all phones without activity within the past week
-        Phone::where('updated_at', '<', now()->subWeek())->delete();
-
-        // Delete all Guest phones without activity within the past day
-        User::Guest()->phones()->where('updated_at', '<', Carbon::now()->subDay())->delete();
-    }
-
-    /**
-     * Show all phones for the logged-in user
-     */
-    public function showPhones()
-    {
-        $this->purgePhones();
-
-        $phones = Phone::where('user_id', Auth::user()->id)
-            ->orWhere('user_id', User::guestId())
-            ->orderBy('created_at', 'desc')
-            ->orderBy('id', 'desc')
-            ->get();
-
-        return View::make('phones', array('phones' => $phones, 'success' => Session::get('success')));
-    }
-
-    /**
-     * Show all locations for given phone
-     * Verify phone belongs to user or guest
-     */
-    public function showPhone(Phone $phone)
-    {
-        if( !($phone->user->id == Auth::user()->id || $phone->user->id == User::guestId()) ) {
-            App::abort(401, 'You are not authorized.');
-        }
-
-        return View::make('phone', array('phone' => $phone));
-    }
-
 }
