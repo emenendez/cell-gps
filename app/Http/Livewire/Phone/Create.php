483fe8d6961e6b28a26301bb0f1d940ed0e4849e
Chris Thompson <christhompsontldr@gmail.com>
2020-06-22 07:45:32 -0400

    Adding ability to customize message

--- app/Http/Livewire/Phone/Create.php
+++ app/Http/Livewire/Phone/Create.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Phone;
 
+use App\Models\Message;
 use App\Models\Phone;
 use Exception;
 use Livewire\Component;
@@ -11,10 +12,13 @@ class Create extends Component
 {
     public $number;
 
+    public $message;
+
     public function save()
     {
         $validatedData = $this->validate([
-            'number' => ['required', 'phone:US'],
+            'number'  => ['required', 'phone:US'],
+            'message' => ['nullable', 'string', 'max:100'],
         ]);
 
         $phone         = new Phone;
@@ -24,14 +28,25 @@ class Create extends Component
 
         $phone->save();
 
-        try {
+        if (empty($this->message)) {
+            $this->message = 'Tap link to send location to SAR: ';
+        }
+
+        $this->message = trim($this->message) . ' ' . route('phones.show', $phone);
+
+        $message          = new Message;
+        $message->message = $this->message;
+
+        $phone->messages()->save($message);
+
+/*        try {
             $client = new Client(config('services.twilio.account_sid'), config('services.twilio.auth_token'));
 
             $client->messages->create(
                 $phone->number,
                 [
                     'from' => config('services.twilio.number'),
-                    'body' => 'Tap link to send location to SAR: ' . route('phones.show', $phone),
+                    'body' => $this->message,
                 ]
             );
         } catch (Exception $e) {
@@ -40,7 +55,11 @@ class Create extends Component
             session()->flash('error', 'Could not send SMS.');
 
             return redirect()->route('manage.dashboard');
-        }
+        }*/
+
+        // reset the form
+        $this->message = '';
+        $this->number  = '';
 
         $this->emit('phoneAdded');
     }
--- resources/views/livewire/phone/create.blade.php
+++ resources/views/livewire/phone/create.blade.php
@@ -2,6 +2,11 @@
     <form wire:submit.prevent="save">
         <x-form-bs name="number" label="Phone Number" required livewire/>
 
-        <button type="submit" class="btn btn-primary">Send Request</button>
+        <x-form-bs name="message" type="textarea" label="Message" livewire :options="['maxlength' => 100, 'rows' => 3]" helper="A link to this service is appended automatically. Leave blank to send the default message."/>
+
+        <button type="submit" class="btn btn-primary">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" wire:loading wire:target="save"></span>
+            <span wire:loading.class="d-none" wire:target="save">Send Request</span>
+        </button>
     </form>
 </div>
